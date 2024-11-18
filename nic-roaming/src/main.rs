743caf3138e39e7ea0a2f998483074adae785c4d
Fabio Mierlo <fmierlo@users.noreply.github.com>
2024-11-18 13:38:23 -0300

    Get nic name from env::args

--- nic-roaming/src/main.rs
+++ nic-roaming/src/main.rs
@@ -1,11 +1,11 @@
 use platform::nic;
 
 fn main() {
-    let name = "en0";
+    let name = std::env::args().nth(1).unwrap();
 
     let nic = nic::new();
 
-    let mac_address = nic.get_mac_address(name);
+    let mac_address = nic.get_mac_address(&name);
 
     println!("nic={:?} mac_address={}", name, mac_address);
 }
