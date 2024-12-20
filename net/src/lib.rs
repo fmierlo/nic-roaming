ca4672d79ea2e462a3fc7d009e54c3a0169f6e91
Fabio Mierlo <fmierlo@users.noreply.github.com>
2024-12-20 16:40:07 -0300

    Use cfg_attr on net os abstraction and general visibility review

--- net/src/lib.rs
+++ net/src/lib.rs
@@ -1,15 +1,13 @@
 #[cfg(not(any(target_os = "linux", target_os = "macos")))]
 compile_error!("Unsupported platform!");
 
-mod ifname;
 mod lladdr;
 #[cfg_attr(target_os = "linux", path = "linux.rs")]
 #[cfg_attr(target_os = "macos", path = "macos.rs")]
 mod os;
 
-pub use ifname::IfName;
 pub use lladdr::{LLAddr, LinkLevelAddress};
-pub use os::Nic;
+pub use os::{IfName, Nic};
 use std::result;
 
 pub type Result<T> = result::Result<T, Box<dyn std::error::Error>>;
